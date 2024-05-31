62d9c0162cce30af91ff0facbcee0736aed92781
William Shen <william.shen.0001@gmail.com>
2024-05-30 23:39:58 -0700

    Added Business Info Column

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -21,9 +23,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             BusinessCardTheme {
                 Surface (){
-
+                    BusinessInfo(name = "TEST", email = "TEST", social = "TEST")
                 }
             }
         }
     }
+}
+
+@Composable
+fun BusinessInfo(name: String, email: String, social: String, modifier: Modifier = Modifier){
+    Column (verticalArrangement = Arrangement.Bottom,
+        modifier = modifier){
+        Text(text = name)
+        Text(text = email)
+        Text(text = social)
+    }
+}
+
+@Preview
+@Composable
+fun BusinessPreview(){
+    BusinessInfo(name = "TEST", email = "TEST", social = "TEST")
 }
