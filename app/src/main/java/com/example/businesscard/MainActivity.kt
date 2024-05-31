a64a7f5648f4431a62a3e82ec74905a068121090
KyleKekawa <104484279+KyleKekawa@users.noreply.github.com>
2024-05-30 23:36:42 -0700

    commit2

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,4 +31,28 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+    @Composable
+    fun BusinessInfo(name: String, title: String, modifier: Modifier = Modifier) {
+        Column (modifier = modifier,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            // Image()
+            Text(name,
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(title,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun BusinessInfoPreview() {
+        BusinessInfo("Dominique Legaspi", "Software Engineer")
+    }
+
 }
