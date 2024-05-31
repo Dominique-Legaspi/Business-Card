2f10d0824122bf00660a457dc361851ebf996efc
DomLegaspi <151315118+Dominique-Legaspi@users.noreply.github.com>
2024-05-31 00:35:12 -0700

    Added BusinessCard function to arrange BusinessInfo/Bottom together with Spacer

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
@@ -25,11 +29,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BusinessCardTheme {
-                Surface (){
+                Surface (modifier = Modifier.fillMaxSize()){
+                    BusinessCard(name ="Dominique Legaspi",
+                        title = "Software Engineer",
+                        phoneNumber = "111-111-1111",
+                        email = "test@email.com",
+                        social = "@YoMama")
                 }
             }
         }
     }
+
+    @Composable
+    fun BusinessCard(name: String, title: String, phoneNumber: String, email: String, social: String, modifier: Modifier = Modifier) {
+        Column (modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally){
+                BusinessInfo(name = name, title = title)
+                Spacer(modifier = Modifier.padding(100.dp))
+                BusinessBottom(phoneNumber = phoneNumber, email = email, social = social)
+                Spacer(modifier = Modifier.padding(30.dp))
+        }
+
+    }
+
     @Composable
     fun BusinessInfo(name: String, title: String, modifier: Modifier = Modifier) {
         Column (modifier = modifier,
@@ -41,6 +64,7 @@ class MainActivity : ComponentActivity() {
                 fontSize = 35.sp,
                 fontWeight = FontWeight.Bold
             )
+            Spacer(modifier = Modifier.padding(5.dp))
             Text(title,
                 fontSize = 15.sp,
                 fontWeight = FontWeight.Bold
@@ -48,21 +72,26 @@ class MainActivity : ComponentActivity() {
         }
     }
     @Composable
-    fun BusinessBottom(name: String, email: String, social: String, modifier: Modifier = Modifier){
-        Column (verticalArrangement = Arrangement.Bottom,
-            modifier = modifier){
-            Text(text = name)
-            Text(text = email)
-            Text(text = social)
+    fun BusinessBottom(phoneNumber: String, email: String, social: String, modifier: Modifier = Modifier){
+        Column (modifier = modifier,
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.Start) {
+                Text(text = phoneNumber)
+                Spacer(modifier = Modifier.padding(5.dp))
+                Text(text = email)
+                Spacer(modifier = Modifier.padding(5.dp))
+                Text(text = social)
         }
     }
 
     @Preview(showBackground = true)
     @Composable
     fun BusinessInfoPreview() {
-        BusinessInfo("Dominique Legaspi", "Software Engineer")
-        BusinessBottom("Dominique Legaspi", "Test@Email.com", "Yo Mama")
-
+        BusinessCard(name ="Dominique Legaspi",
+            title = "Software Engineer",
+            phoneNumber = "111-111-1111",
+            email = "test@email.com",
+            social = "@YoMama")
     }
 
 }
