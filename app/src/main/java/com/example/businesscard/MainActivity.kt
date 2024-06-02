93d8ef0bdb6d5c7b620cf67c3ca05bd96785650f
DomLegaspi <151315118+Dominique-Legaspi@users.noreply.github.com>
2024-06-01 19:57:22 -0700

    added background image, icons, profile image

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,20 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,8 +39,8 @@ class MainActivity : ComponentActivity() {
             BusinessCardTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     BusinessCard(
-                        name = "Dominique Legaspi",
-                        title = "Software Engineer",
+                        name = "Phuong Nguyen",
+                        title = "Professor of Mobile App Development",
                         phoneNumber = "111-111-1111",
                         email = "test@email.com",
                         social = "@YoMama"
@@ -43,47 +51,144 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// Full Business Card
 @Composable
-fun BusinessCard(name: String, title: String, phoneNumber: String, email: String, social: String, modifier: Modifier = Modifier) {
-    Column (modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Bottom,
-        horizontalAlignment = Alignment.CenterHorizontally){
-            BusinessInfo(name = name, title = title)
-            Spacer(modifier = Modifier.padding(100.dp))
-            BusinessBottom(phoneNumber = phoneNumber, email = email, social = social)
-            Spacer(modifier = Modifier.padding(30.dp))
+fun BusinessCard(
+    name: String,
+    title: String,
+    phoneNumber: String,
+    email: String,
+    social: String,
+    modifier: Modifier = Modifier
+) {
+    // Background image with opacity
+    Box(modifier) {
+        Image(
+            painter = painterResource(R.drawable.pyramid_bg),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        // Set white opacity
+        Surface(
+            color = Color.White.copy(alpha = 0.65f),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            // Arrange Composable into columns
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                BusinessPhoto()
+                Spacer(modifier = Modifier.padding(10.dp))
+                BusinessInfo(name = name, title = title)
+                Spacer(modifier = Modifier.padding(70.dp))
+                BusinessBottom(phoneNumber = phoneNumber, email = email, social = social)
+                Spacer(modifier = Modifier.padding(30.dp))
+            }
+        }
     }
-
 }
 
+// Profile image
+@Composable
+fun BusinessPhoto() {
+    Image(
+        painter = painterResource(R.drawable.phuong),
+        contentDescription = "Profile image",
+        modifier = Modifier
+            .size(240.dp)
+            .clip(CircleShape)
+    )
+}
+
+// Business Card information
 @Composable
 fun BusinessInfo(name: String, title: String, modifier: Modifier = Modifier) {
-    Column (modifier = modifier,
+    Column(
+        modifier = modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        // Image()
-        Text(name,
-            fontSize = 35.sp,
-            fontWeight = FontWeight.Bold
+    ) {
+        // full name parameter
+        Text(
+            name,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
         )
         Spacer(modifier = Modifier.padding(5.dp))
-        Text(title,
-            fontSize = 15.sp,
-            fontWeight = FontWeight.Bold
+        // title parameter
+        Text(
+            title,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
         )
     }
 }
+
+// Contact information
 @Composable
-fun BusinessBottom(phoneNumber: String, email: String, social: String, modifier: Modifier = Modifier){
-    Column (modifier = modifier,
+fun BusinessBottom(
+    phoneNumber: String,
+    email: String,
+    social: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
         verticalArrangement = Arrangement.Bottom,
-        horizontalAlignment = Alignment.Start) {
-            Text(text = phoneNumber)
-            Spacer(modifier = Modifier.padding(5.dp))
-            Text(text = email)
-            Spacer(modifier = Modifier.padding(5.dp))
-            Text(text = social)
+        horizontalAlignment = Alignment.Start
+    ) {
+        // Phone Number
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.phone_icon),
+                contentDescription = "phone number",
+                modifier = Modifier.size(30.dp)
+            )
+            Spacer(modifier = Modifier.padding(horizontal = 5.dp))
+            Text(
+                text = phoneNumber,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+        Spacer(modifier = Modifier.padding(5.dp))
+        // Email
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.email_icon),
+                contentDescription = "email",
+                modifier = Modifier.size(30.dp)
+            )
+            Spacer(modifier = Modifier.padding(horizontal = 5.dp))
+            Text(
+                text = email,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+        Spacer(modifier = Modifier.padding(5.dp))
+        // LinkedIn
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.linkedin_icon),
+                contentDescription = "linkedin",
+                modifier = Modifier.size(30.dp)
+            )
+            Spacer(modifier = Modifier.padding(horizontal = 5.dp))
+            Text(
+                text = social,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
     }
 }
 
@@ -91,8 +196,8 @@ fun BusinessBottom(phoneNumber: String, email: String, social: String, modifier:
 @Composable
 fun BusinessInfoPreview() {
     BusinessCard(
-        name = "Dominique Legaspi",
-        title = "Software Engineer",
+        name = "Phuong Nguyen",
+        title = "Professor of Mobile App Development",
         phoneNumber = "111-111-1111",
         email = "test@email.com",
         social = "@YoMama"
